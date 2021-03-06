bc705673113f084f63147956a513dbc4b6724e65
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2021-03-06 16:52:32 +0200

    Add event & issue to watering admin

--- watering/admin.py
+++ watering/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from watering.models import Event, Issue
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("box_id", "start_date", "end_date", "consumption", )
+    list_filter = ("box_id", )
+    ordering = ("-start_date", )
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("box_id", "submitted_by", "issue_type", "created", "resolved", )
+    list_filter = ("box_id", "issue_type", "resolved", )
+    ordering = ("-created", )
