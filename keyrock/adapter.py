3b763956fb2326b96936127ee5a81db93f14d8fb
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2021-10-12 22:40:27 +0300

    Only link existing users to keyrock

--- keyrock/adapter.py
+++ keyrock/adapter.py
@@ -7,11 +7,12 @@ class KeyRockAdapter(DefaultSocialAccountAdapter):
 
     def pre_social_login(self, request, sociallogin):
         # try to connect with existing user from local database
-        try:
-            user = User.objects.get(username=sociallogin.user.username)
-            sociallogin.connect(request, user)
-        except User.DoesNotExist:
-            pass
+        if request.user.pk:
+            try:
+                user = User.objects.get(username=sociallogin.user.username)
+                sociallogin.connect(request, user)
+            except User.DoesNotExist:
+                pass
 
         # mark test-user as admin
         if sociallogin.user.email == 'test-user@example.com':
