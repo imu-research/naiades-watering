93351392f9e18b96ea440c4c86c8f2bf3ecc3665
EvangelieAnagnostopoulou <evangelie_5@hotmail.com>
2022-02-06 18:58:59 +0200

    Fix keyrock adapter

--- keyrock/adapter.py
+++ keyrock/adapter.py
@@ -1,3 +1,4 @@
+from allauth.account.utils import perform_login
 from django.contrib.auth.models import User
 
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
@@ -6,14 +7,19 @@ from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 class KeyRockAdapter(DefaultSocialAccountAdapter):
 
     def pre_social_login(self, request, sociallogin):
-        # try to connect with existing user from local database
-        if request.user.pk:
-            try:
-                user = User.objects.get(username=sociallogin.user.username)
-                sociallogin.connect(request, user)
-            except User.DoesNotExist:
-                pass
+        # ignore if user exists
+        if sociallogin.user.id:
+            return
+
+        # try to connect based on username
+        try:
+            user = User.objects.get(username=sociallogin.user.username)
+            sociallogin.state['process'] = 'connect'
+            perform_login(request, user, 'none')
+        except User.DoesNotExist:
+            pass
 
         # mark test-user as admin
         if sociallogin.user.email == 'test-user@example.com':
             sociallogin.user.is_staff = sociallogin.user.is_superuser = True
+
